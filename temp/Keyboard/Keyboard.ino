69ab3716db1b5fc7ed90d2f43611fecb9b2b3363
dofinsun <dofin.sun@gmail.com>
2017-09-13 15:10:01 +0200

    Add files via upload

---
+++ temp/Keyboard/Keyboard.ino
@@ -0,0 +1,18 @@
+#include "Keyboard.h"
+
+void setup() {
+  Keyboard.begin();
+}
+
+void loop() {
+  delay(3000);
+  Keyboard.print("user");
+  Keyboard.write(KEY_RETURN);
+  delay(3000);
+  Keyboard.write(KEY_F12);
+  while (1) {
+    ;  
+  }
+}
+
+
---
+++ temp/RUtest05_spinfc3_i2cM/RUtest05_spinfc3_i2cM.ino
@@ -0,0 +1,28 @@
+#include <Wire.h>
+
+void setup() {
+  Wire.begin();        // join i2c bus (address optional for master)
+  Serial.begin(9600);  // start serial for output
+  while (!Serial) {
+    ;
+  }
+}
+
+void loop() {
+  for (int i = 0; i < 3; i++) {
+    Wire.beginTransmission(16);
+    Wire.write(i);
+    Wire.endTransmission();
+    delay(10);
+    Serial.print(i);
+    Serial.print(" ");
+    Wire.requestFrom(16, 4);
+    while (Wire.available()) {
+      byte c = Wire.read();
+      Serial.print(String(c,HEX));
+    }
+    Serial.println();
+    delay(10);
+  }
+  delay(900);
+}
